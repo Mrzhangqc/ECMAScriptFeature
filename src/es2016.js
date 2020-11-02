e9cdf7b9f66cb860dae26969a051a050d9648c69
Mr_zhang <qichang.zhang@zhangmen.com>
2020-11-02 16:16:52 +0800

    feat: 添加es2016新功能

---
+++ src/es2016.js
@@ -0,0 +1,16 @@
+/* es2016 新功能 */
+
+//Exponentiation Operator
+  //求幂运算符
+  console.log("---------- Exponentiation Operator(求幂运算符) -----------")
+  console.log("2的3次平方，使用Math方法(Math.pow(2,3))：", Math.pow(2,3));
+  console.log("2的3次平方，使用求幂运算符2**3)", 2**3);
+  console.log('--------------------------------', '\n')
+
+//(Array/String).prototype.includes
+console.log("---------- (Array/String).prototype.includes -----------")
+const originArray = ["test1", "test2"]
+const originString = "test1test2"
+console.log(`${JSON.stringify(originArray)} includes('test1'): `, originArray.includes("test1"));
+console.log(`${JSON.stringify(originString)} includes('test1'): `, originString.includes("test1"));
+console.log('--------------------------------', '\n')
